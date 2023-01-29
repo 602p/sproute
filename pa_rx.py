22e879fa678a521f48283269b6a55ee9e816b99a
Louis Goessling <louis@goessling.com>
2023-01-28 22:52:38 -0600

    asd

--- pa_rx.py
+++ pa_rx.py
@@ -1,6 +1,7 @@
 import array
 import math
 import time
+import os
 
 import pyaudio
 
@@ -17,13 +18,24 @@ BLKSZ = 128
 t = 0
 i = 0
 
-win = [0] * 48
 
-clk_time = 0.1
+clk_time = 0.03
+
+time_per_sample = BLKSZ / fs
+
+win = [0] * int(clk_time / time_per_sample / 2)
+
+print('winlen:', len(win))
 
 frame_start = 0
 in_frame = False
 frame_bits = []
+
+
+assert(len(win) * time_per_sample < clk_time * 0.5)
+
+msg = ""
+
 
 while 1:
     block = stream.read(BLKSZ)
@@ -36,23 +48,32 @@ while 1:
     win.insert(0, bit)
     del win[-1]
 
-    debounced = all(win)
+    debounced = all(win) #sum(win) > len(win) * 0.75
 
     if debounced and not in_frame:
+        # print("Start Frame")
         in_frame = True
-        frame_start = t
+        frame_start = t + (clk_time + time_per_sample)
 
-    if in_frame and (t - frame_bits) > (len(frame_bits) * clk_time):
-        frame_bits.append(not debounced)
+    if in_frame and ((t - frame_start) > ((len(frame_bits) * clk_time) + (clk_time / 2))):
+        # print("Bit:", debounced)
+        # print(win)
+        frame_bits.append(debounced)
 
     if len(frame_bits) == 8:
         in_frame = False
-        print(frame_bits)
+        num = sum(x<<n for x, n in zip(frame_bits, range(0, len(frame_bits))))
+        # print("End Frame:", frame_bits, num, chr(num))
+        if num:
+            msg += chr(num)
+            os.system("clear")
+            print(msg)
+        frame_bits = []
 
     # if (i % 10) == 0:
     #     print(f'{t: 2.2f} {debounced}')
 
-    t += BLKSZ / fs
+    t += time_per_sample
     i += 1
 
 stream.stop_stream()
