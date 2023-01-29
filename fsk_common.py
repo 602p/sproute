108482431d0c85e200b4fc1d6b5aba485f7ed8c7
Louis Goessling <louis@goessling.com>
2023-01-29 04:45:57 -0600

    arr

--- fsk_common.py
+++ fsk_common.py
@@ -8,8 +8,8 @@ BLKSZ = 1024 * 4
 
 bit_clk = (BLKSZ / sr)
 
-min_freq = 500
-max_freq = 2500
+min_freq = 400
+max_freq = 2800
 
 freq = np.fft.fftfreq(BLKSZ, d=1/sr)
 start = np.argmax(freq > min_freq)
@@ -26,6 +26,10 @@ tones = list(range(min_freq+tone_step, max_freq, tone_step))
 # tones = tones[:8]
 print(len(tones), 'tones')
 
+simul_tones = 3
+print(simul_tones, 'simul tones')
+
+tone_bin_size = tone_step / 2
 
 for t in tones:
     n = 0
@@ -34,12 +38,6 @@ for t in tones:
             n += 1
     print("Tone", t, "->", n, "bins")
 
-
-simul_tones = 3
-print(simul_tones, 'simul tones')
-
-tone_bin_size = tone_step / 2
-
 symbols = [set(x) for x in itertools.combinations(tones[:-1], simul_tones)]
 clock_tone = tones[-1]
 
@@ -47,7 +45,7 @@ print('clock tone:', clock_tone)
 
 print(len(symbols), 'combinations;', math.log2(len(symbols)), 'bits')
 # symbols = symbols[:256]
-assert(len(symbols) >= 128)
+assert(len(symbols) >= 256)
 
 print(len(symbols))
 
